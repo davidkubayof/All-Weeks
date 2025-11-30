fb80e7216598b8e58ddad00460db4a80849379cc
David Kubayof <davidkubayof@gmail.com>
2025-11-30 20:26:03 +0200

    up if.js to git

---
+++ week7/day1/if.js
@@ -0,0 +1,139 @@
+//exe 1
+// let score = 85;
+// if(score>60){console.log("passed the test:")};
+
+// //exe 2
+// let temperature = 32;
+// if(temperature>30){console.log('it is not today: ')}
+
+//exe 3
+// let number = 14;
+// if(number%2==0){console.log("the num is even:  ")}
+
+//exe 4
+// let balance = -5;
+// if(balance<0){console.log("Warning: negative balance")}
+
+//exe 5
+// let score = 120;
+// if(score>100){console.log("High score bonus: ")}
+
+//exe 6
+// const inputText="";
+// if(inputText==""){console.log("no input provid: ")}
+
+//exe 7
+// const age=21;
+// if(age>18){console.log("User is an adult")}
+
+//exe 8
+// const speed=95;
+// if(speed>90){console.log("Speeding!")}
+
+//exe 9
+// const stock=0;
+// if(stock==0){console.log("Out of stock")}
+
+//exe 10
+// const grade = 93;
+// if(grade>90){console.log("Grade A")}
+
+//exe 11
+// let score = 58;
+// if(score>60){console.log("pass")}
+// else {console.log("fail")}
+
+//exe 12
+// let number = 11;
+// if(number%2==0)
+//     {console.log("Even number")}
+// else {console.log("Odd number")}
+
+//exe 13
+// let age = 10;
+// if(age<12)
+//     {console.log("child price")}
+// else {console.log("adult price")}
+
+//exe 14
+// let speed = 50;
+// if(speed>80) {console.log("to fast")}
+// else{console.log("speed ok")}
+
+//exe 15
+// let userName = "admin";
+// if(userName==="admin"){console.log("login successful")}
+// else{console.log("Login failed")}
+
+//exe 16
+// let number = 0;
+// if(number>0){console.log("Positive")}
+// else{"Zero or negative"}
+
+// let stock = 5;
+// if(stock>0){console.log("Item available")}
+// else{console.log("Item sold out")}
+
+//exe 17
+// let message = "hello";
+// if(message.length>3){console.log("Long text")}
+// else{console.log("Short text")}
+
+//exe 18
+// let password = "1234";
+// if(password="9999"){console.log("Access granted: ")}
+// else {console.log("Wrong password: ")}
+
+//exe 19
+// let value=7;
+// if(value>1 && value<10){console.log("Inside range")}
+// else{console.log("Outside range")}
+
+//exe 20
+// let grade = 88;
+// if(grade>90){console.log("A")}
+// else if(grade>80){console.log("B")}
+// else {console.log("c or below")}
+
+//exe 21
+// let temperature = 18;
+// if(temperature<10){console.log("is cold: ")}
+// else if(temperature<25){console.log("nice")}
+// else {console.log("hot")}
+
+//exe 22
+// let speed = 0;
+// if(speed===0){console.log("stopped")}
+// else if(speed<60){console.log("normal speed")}
+// else{console.log("too fast")}
+
+//exe 23
+// let score=52;
+// if(score<50){console.log("bad")}
+// else if(score>50&&score<=79){console.log("ok")}
+// else {console.log("excellent")}
+
+//exe 24
+// let age = 13;
+// if(age<13){console.log("child")}
+// else if(age>=13&&age<=17){console.log("teenager")}
+// else{console.log("adult")}
+
+//exe 25
+// let health = 25;
+// if(health>70){console.log("Health: high")}
+// else if(health>=30 && health<=70){console.log("Health:medium")}
+// else {console.log("Health: low")}
+
+//exe 26
+// let grade = 45;
+// if(grade<50){console.log("failed:")}
+// else if(grade>=50 && grade<=79){console.log("passed")}
+// else{console.log("excellent: ")}
+
+//exe 27
+// let hour = 14;
+// if(hour>=0&&hour<=5){console.log("night:")}
+// else if(hour>6 && hour<11){console.log("morning:")}
+// else if(hour>12 && hour<17){console.log("afternoon:")}
+// else{"evening:"}
